bd5a359e89ea564b01651dd9e4e309c2f31e51f7
LCR497 <abirov999@gmail.com>
2022-09-01 20:13:15 +0600

    Change models, admin panel

--- instagram/models.py
+++ instagram/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 
+
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    avatar = models.ImageField()
-    password = models.CharField()
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
     email = models.EmailField()
-    phone = models.CharField(max_length=13)
-    followers = models.IntegerField()
-    follow = models.IntegerField()
-    
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    name = models.OneToOneField(User,on_delete=models.CASCADE)
+    avatar = models.ImageField(null=True, blank=True)
+    followers = models.IntegerField(default=0)
+    follow = models.IntegerField(default=0)
+
 class Post(models.Model):
-    author = models.ForeignKey(User.name)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     image = models.ImageField()
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     likes = models.BooleanField(default=False)
